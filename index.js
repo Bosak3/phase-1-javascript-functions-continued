ec16e2279d010d2d3c8ffd74dd604f2df69d2252
Adam <bosak3@gmail.com>
2021-10-14 12:20:28 -0500

    commit tree 96ad2d14fd5d237b8e032231ae0fea6f4999ef6d

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+const activity2 = "I want to bathe my dog";
+
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+const noBlues = 'work from home';
+
+function mondayWork (mondayBlues = "go to the office"){
+    return (`This Monday, I will ${mondayBlues}.`);
+}
+
+function wrapAdjective(param1 = '*'){
+    
+}
